3b35add4efb181b6f0e0cc2a7ab62f2ab16cd575
mik-tsujihara <k-tsujihara@mik-s.jp>
2025-04-11 11:18:38 +0900

    Update springboot/demo/src/main/java/com/example/demo/controller/HelloController.java
    Co-authored-by: mik-yoshioka <67355045+mikyoshioka@users.noreply.github.com>

--- springboot/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ springboot/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-  @GetMapping("/")
+  @GetMapping("/helloworld")
   public String hello() {
     return "Hello World";
   }
