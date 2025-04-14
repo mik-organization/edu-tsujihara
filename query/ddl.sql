8c166fc32d61bea029cf7a69989740824fab1e3b
mik-tsujihara <k-tsujihara@mik-s.jp>
2025-04-14 11:54:10 +0900

    reviewテーブル追加

--- query/ddl.sql
+++ query/ddl.sql
@@ -4,9 +4,14 @@ id INT,
 title CHAR(30),
 contents VARCHAR(100),
 category VARCHAR(100),
-review VARCHAR(256),
 PRIMARY KEY(id)
 );
 
+CREATE TABLE review(
+id int,
+video_id int REFERENCES video(id),
+review VARCHAR(256),
+user_name VARCHAR(100)
+);
 
 
